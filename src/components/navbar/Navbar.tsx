a92dceaa6b811bb7cf97ebb5263f2f037685a362
Martinelii <Kevenmartineli@outlook.com>
2025-01-27 11:38:44 -0300

    feature: ajustanto rotas home

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom"
+
 function Navbar(){
     return(
         <>
             <div className="w-full flex justify-center py-4 bg-slate-600 text-white">
 
                 <div className="container flex justify-between text-lg">
-                    Farmacia
+                    <Link to='/home'className= "text 2xl font-bold">
+                        Farma 77
+                    </Link>
 
                     <div className="flex gap-4">
                         Produtos
